48dfbe6eb581a9ed99cf46a8519b55b6ca4f890a
Lior Sabag <lior@trendiguru.com>
2017-04-14 19:32:07 +0300

    Add client side notifications support

--- client/SimpleChat/index.ios.js
+++ client/SimpleChat/index.ios.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  View
+  View,
+  PushNotificationIOS,
+  AlertIOS
 } from 'react-native';
 import SocketIOClient from 'socket.io-client';
 import { GiftedChat } from 'react-native-gifted-chat';
@@ -12,8 +14,9 @@ export default class SimpleChat extends Component {
     this.state = { messages: [] };
     this.onSend = this.onSend.bind(this);
     this.onChatReceived = this.onChatReceived.bind(this);
+    this._onRegistered = this._onRegistered.bind(this);
 
-    this.socket = SocketIOClient('http://localhost:5050/');
+    this.socket = SocketIOClient('https://9494294e.ngrok.io');
     this.socket.on('chat', (message) => {
       this.setState((previousState) => {
         return { messages: GiftedChat.append(previousState.messages, message) };
@@ -33,6 +36,19 @@ export default class SimpleChat extends Component {
           },
         }],
     });
+
+    PushNotificationIOS.addEventListener('register', this._onRegistered);
+    PushNotificationIOS.addEventListener('registrationError', this._onRegistrationError);
+    PushNotificationIOS.addEventListener('notification', this._onRemoteNotification);
+    PushNotificationIOS.addEventListener('localNotification', this._onLocalNotification);
+    PushNotificationIOS.requestPermissions();
+  }
+
+  componentWillUnmount() {
+    PushNotificationIOS.removeEventListener('register', this._onRegistered);
+    PushNotificationIOS.removeEventListener('registrationError', this._onRegistrationError);
+    PushNotificationIOS.removeEventListener('notification', this._onRemoteNotification);
+    PushNotificationIOS.removeEventListener('localNotification', this._onLocalNotification);
   }
 
   onChatReceived(message) {
@@ -56,6 +72,52 @@ export default class SimpleChat extends Component {
       />
     );
   }
+
+
+  _onRegistered(deviceToken) {
+    console.log(deviceToken);
+    this.onChatReceived({
+      _id: 1,
+      text: `Registered for Notifications with token: ${deviceToken}`,
+      createdAt: new Date(),
+      user: {
+        _id: 'admin',
+        name: 'Admin Admin',
+        avatar: 'https://facebook.github.io/react/img/logo_og.png',
+      },
+    });
+    AlertIOS.alert(
+      'Registered For Remote Push',
+      `Device Token: ${deviceToken}`,
+      [{
+        text: 'Dismiss',
+        onPress: null,
+      }]
+    );
+  }
+
+  _onRegistrationError(error) {
+    AlertIOS.alert(
+      'Failed To Register For Remote Push',
+      `Error (${error.code}): ${error.message}`,
+      [{
+        text: 'Dismiss',
+        onPress: null,
+      }]
+    );
+  }
+
+  _onRemoteNotification(notification) {
+    AlertIOS.alert(
+      'Push Notification Received',
+      'Alert message: ' + notification.getMessage(),
+      [{
+        text: 'Dismiss',
+        onPress: null,
+      }]
+    );
+  }
+
 }
 
 AppRegistry.registerComponent('SimpleChat', () => SimpleChat);
