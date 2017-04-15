cafd53212743303dfb53d39e265075ecf19e4df3
Lior Sabag <lior@trendiguru.com>
2017-04-15 04:36:59 +0300

    Improve notification functionality + minor fixes.

--- server/app.py
+++ server/app.py
@@ -4,15 +4,20 @@ monkey.patch_all()
 import uuid
 import json
 from datetime import datetime
+from collections import defaultdict
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
-from . import notifier
+import notifier
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 connected_clients = set()
-registered_tokens = {}
+
+# Keep track of token/session relationships for push notifications.
+# If a token has no connected sessions it should be sent a push notification.
+registered_token_sessions = defaultdict(set)
+session_tokens = {}
 
 
 @app.route('/')
@@ -29,30 +34,40 @@ def web_chat():
 def broadcast():
     """
     Allows triggering from other processes/servers, e.g.:
-    http://localhost:5000/broadcast?message=Hello%20World!
+    http://localhost:5050/broadcast?message=Hello%20World!
     """
     if request.method == 'GET':
-        message = request.args.get('message')
+        text = request.args.get('message')
     else:
-        message = json.loads(request.data).get('message')
-    socketio.emit('chat', new_message(message))
+        text = json.loads(request.data).get('message')
+    message = new_message(text)
+    socketio.emit('chat', message)
+    notify_disconnected(message)
     return "Broadcast \"{0}\" to {1} devices.".format(message, len(connected_clients))
 
 
 @socketio.on('connect')
 def on_connect():
     connected_clients.add(request.sid)
+    print "{} connected".format(request.sid)
     # Use `room` arg to send only to user who just connected
     socketio.emit('get-id', str(request.sid), room=str(request.sid))
 
+
 @socketio.on('register')
 def on_register(token):
-    registered_tokens[request.id] = token
+    registered_token_sessions[token].add(request.sid)
+    session_tokens[request.sid] = token
+    print registered_token_sessions
 
 
 @socketio.on('disconnect')
 def on_disconnect():
     connected_clients.remove(request.sid)
+    token = session_tokens.pop(request.sid, None)
+    if token:
+        registered_token_sessions[token].remove(request.sid)
+    print "{} disconnected".format(request.sid)
 
 
 @socketio.on('chat')
@@ -61,9 +76,24 @@ def on_chat(message):
     if "_id" not in message:
         message["_id"] = str(uuid.uuid1())
 
+    # This is giving an error. TODO: find alternative to offload notification as it can hang
+    # notify_thread = socketio.server.start_background_task(notify_disconnected, message)
     socketio.emit('chat', message)
-    for token, sid in registered_tokens.iteritems():
-        if sid not in connected_clients:
+    notify_disconnected(message)
+
+
+def notify_disconnected(message):
+    """
+    Figure out which devices are not currently connected and notify them of the new message.
+    This is not strictly necessary (we could just notify everyone all the time), but greatly
+    reduces the number of notifications. Also, notification timing is not deterministic, so
+    this can prevent situations where users receive delayed notification for messages they've
+    already seen after they leave the app.
+    """
+    for token, sid_list in registered_token_sessions.iteritems():
+        all_disconnected = all(not socketio.server.manager.is_connected(sid, request.namespace)
+                               for sid in sid_list)
+        if all_disconnected:
             notifier.send_message(token, message)
 
 
--- server/notifier.py
+++ server/notifier.py
@@ -2,15 +2,19 @@ import os
 from apns import APNs, Payload
 
 test_token = "8d5a5b8232248d2d820d6324c6865adbbfe118ae866b3e6680177e43fe9dbb34"
-# old_cert_path./production_im.subl.reactjs.native.SimpleChat.pem"
+# old_cert_path = ./production_im.subl.reactjs.native.SimpleChat.pem"
 
 CERT_PATH = os.environ["CERT_PATH"]
 
 apn_service = APNs(use_sandbox=True, cert_file=CERT_PATH)
 
+print "cert_path: {}, apns connected: {}".format(CERT_PATH, apn_service.gateway_server.connection_alive)
+
 
 def send_message(token, message):
+    print "Sending {} to {}".format(message["text"], token)
     p = Payload(alert="{user[name]}: {text}".format(**message),
-                sound="default", badge=1)
+                sound="default",
+                badge=1)
     apn_service.gateway_server.send_notification(token, p)
-
+    print "Done Sending {} to {}".format(message["text"], token)
